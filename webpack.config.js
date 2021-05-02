e5c92e2614eda8e6d80c15ec90a381263db96a0e
P. Gilbert Schmitt <PGilbertSchmitt@gmail.com>
2021-05-02 12:15:16 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const tsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+var babelOptions = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true
+        }
+      }
+    ],
+    "@babel/preset-typescript",
+    "@babel/preset-react"
+  ],
+  plugins: [
+    "@babel/proposal-class-properties"
+  ]
+};
+
+module.exports = {
+  context: __dirname,
+  target: "web",
+  mode: "development",
+  entry: "src/index.tsx",
+  output: {
+    path: path.resolve('public'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "src"),
+      "node_modules"
+    ],
+    extensions: [".js", ".ts", ".tsx"],
+    plugins: [
+      new tsConfigPathsPlugin()
+    ]
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: "source-map-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: {
+          loader: "eslint-loader",
+          options: {
+            cache: true,
+            failOnError: true,
+            failOnWarning: false
+          }
+        }
+      }
+    ]
+  },
+  node: {
+    __dirname: false,
+  }
+};
